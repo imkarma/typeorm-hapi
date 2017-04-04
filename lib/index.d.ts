6a4a009573c06239408ce84d483425b23762c5ca
imkarma <andrew.pozunyk@gmail.com>
2017-04-04 12:49:58 +0300

    remove class

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,5 +2,6 @@ export interface IRegister {
     register(server: any, options: any, next: any): void;
     attributes?: any;
 }
-declare var _default: () => IRegister;
-export default _default;
+export declare const plugin: {
+    register: (server: any, option: any, next: any) => void;
+};
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ export interface IRegister {
     attributes?: any;
 }
 
-export default ():IRegister =>{
-    return {
-        register: (server, option, next) => {
-            this.attributes = {
-                pkg: require('../package.json')
-            };
-            console.log('Register');
-            next();
-        }
+export const plugin = {
+    register: (server, option, next) => {
+        // this.attributes = {
+        //     pkg: require('../package.json')
+        // };
+        console.log('Register');
+        next();
     }
 }
